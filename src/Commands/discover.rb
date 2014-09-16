2d7ec5f84e12c5ad5c960072143cca3ecf5d6823
rxk8345 <rxk8345@g.rit.edu>
2014-09-16 13:48:49 -0400

    trying to hard code authentication

--- src/Commands/discover.rb
+++ src/Commands/discover.rb
@@ -1,26 +1,39 @@
 require 'rubygems'
 require 'mechanize'
 
-agent = Mechanize.new
-page = agent.get('http://google.com/')
-
-
-puts(page)
-array = Array.new
-form = page.forms() 
-form.each do |f|
-	keys = f.keys
-	puts('keys: ')
-	puts( keys )
-end 
-
-
-def getLinks()
+def getLinks( page )
+	puts("Link: ")
 	page.links.each do |link|
 		array.push(link)
-		#puts( link.text )
+		puts( link.text )
 		#puts( link.href ) 
 	end
 end
+
+def main()
+	agent = Mechanize.new
+	page = agent.get('http://127.0.0.1/dvwa/login.php')
+
+
+	form = page.forms.first 
+
+	puts(form)
+	# keys = form.keys
+	# puts('keys: ')
+	# keys.each do | field |  
+	# 	puts( field )
+	# 	form[field] = 'admin'
+	# end 
+	form['username'] = "admin"
+	form['password'] = "password"
+	puts( form['username'])
+	puts( form['password'])
+	page = agent.submit(form)
+	pp page
+	getLinks(page)
+end
+
+
+main()
 
 
