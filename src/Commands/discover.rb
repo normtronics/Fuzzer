a75eb039cafca474732677071d17dab37d2d4bad
rxk8345 <rxk8345@g.rit.edu>
2014-09-17 14:48:01 -0400

    get fields
    Separated out authentication
    created func to get all fields

--- src/Commands/discover.rb
+++ src/Commands/discover.rb
@@ -4,37 +4,47 @@ require 'mechanize'
 def getLinks( page )
 	puts("Link: ")
 	page.links.each do |link|
-		puts( link.text )
-		#puts( link.href ) 
+		#puts( link.text )
+		puts( link.href ) 
 	end
 end
+
+def customAuth(form, agent)
+
+	form['username'] = "admin"
+	form['password'] = "password"
+	 # Required otherwise form submission fails silently
+    form["Login"] = 'submit'
+    return agent.submit(form)
+
+end
+
+def getFieldsOnPage(form)
+	keys = form.keys
+	puts('keys: ')
+	keys.each do | field |  
+		puts( field )
+	end 
+end
+
 
 def main()
 	agent = Mechanize.new
 	page = agent.get('http://127.0.0.1/dvwa/login.php')
+
 	form = page.forms.first 
+	if(form.has_field?'username' and form.has_field?'password')
+		page = customAuth(form, agent)
+	end
 
-
-	puts(form)
-	# keys = form.keys
-	# puts('keys: ')
-	# keys.each do | field |  
-	# 	puts( field )
-	# 	form[field] = 'admin'
-	# end 
-	form['username'] = "admin"
-	form['password'] = "password"
-	puts( form['username'])
-	puts( form['password'])
-	 # Required otherwise form submission fails silently
-    form["Login"] = 'submit'
-
-	pp page
-	page = agent.submit(form)
-	pp page
+	page.forms.each do | form |
+		getFieldsOnPage(form)
+	end
 	
 	getLinks(page)
 end
+
+
 
 
 main()
