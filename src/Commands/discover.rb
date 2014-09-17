8179edb17ac2cc4563c431d77e58cc1f894a3162
christophernorman <normtronics@hotmail.com>
2014-09-17 11:18:09 -0400

    Pushing to new branch

--- src/Commands/discover.rb
+++ src/Commands/discover.rb
@@ -1,5 +1,96 @@
 require 'rubygems'
 require 'mechanize'
+require 'uri'
+require 'http-cookie'
+
+
+class Discover
+
+	def initialize(commonWords, page)
+		f = File.open(commonWords, "r")
+		@agent = Mechanize.new
+		@page = page
+		@base = getBasePath(page)
+
+		puts @base
+
+		begin
+			@commonArray = Array.new
+			f.each_line do |line|
+  				@commonArray.push(line)
+  				puts line
+			end
+			f.close
+		rescue SystemCallError
+			puts 'IO Failed. Check file path'
+			f.close
+			File.delete(commonWords)
+			raise
+		end	
+
+		athenticate()
+		findLinks()
+		getFormParams()
+		puts getCookies()
+	end
+
+	def findLinks
+		page = @agent.get(@page)
+		pp page
+		linksArray = Array.new
+		page.links.each do |link|
+			linksArray.push(link)
+			puts( link.uri )
+		end
+	end
+
+	def athenticate
+		page = @agent.get(@page)
+		form = page.forms.first 
+
+
+		form['username'] = "admin"
+		form['password'] = "password"
+		page2 = form.click_button
+
+		#Might want to iterate through page to really check if its the same
+		if page2.title.eql?(page.title)
+			puts 'Login did not work'
+		else
+			@page = page2
+		end
+	end
+
+	def pageGuess
+		@commanArray.each do |word|
+			page = @agent.get(@base + '/' + word)
+			puts page
+		end
+	end
+
+	def parseUrl
+
+
+	end
+
+
+	def getFormParams
+		page = @agent.get(@pages)
+		page.forms
+	end
+
+	def getCookies
+		return @agent.cookies.to_s
+	end
+
+	def getBasePath(page)
+		uri = URI(page)
+		return "#{uri.scheme}://#{uri.host}"
+	end
+
+end
+
+
 
 def getLinks( page )
 	puts("Link: ")
@@ -12,28 +103,29 @@ end
 
 def main()
 	agent = Mechanize.new
-	page = agent.get('http://127.0.0.1/dvwa/login.php')
-
+	page = agent.get('http://localhost:8888/dvwa/login.php')
+	pp page
 
 	form = page.forms.first 
 
-	puts(form)
-	# keys = form.keys
-	# puts('keys: ')
-	# keys.each do | field |  
-	# 	puts( field )
-	# 	form[field] = 'admin'
-	# end 
 	form['username'] = "admin"
 	form['password'] = "password"
-	puts( form['username'])
-	puts( form['password'])
-	page = agent.submit(form)
-	pp page
+	page2 = form.click_button
+
+	pp page2
+
+	#might want to iterate through page to really check if its the same
+	if page2.title.eql?(page.title)
+		puts 'Login did not work'
+	end
+
 	getLinks(page)
 end
 
 
-main()
+#main()
+
+
+
 
 
